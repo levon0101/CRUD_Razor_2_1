98228e22ff90997f76774deaf2263957aa569eaa
Levon.Lavr <levon.ecs@gmail.com>
2019-05-08 18:45:07 +0400

    Add project files.

---
+++ CRUD_Razor_2_1/Model/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_Razor_2_1.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ CRUD_Razor_2_1/Model/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace CRUD_Razor_2_1.Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
